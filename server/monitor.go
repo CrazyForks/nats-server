d68aa93a89dc88908ee7c12da049a0c0115a96e8
Maurice van Veen <github@mauricevanveen.com>
2026-03-04 17:30:33 +0100

    WIP: stream peers reporting
    Signed-off-by: Maurice van Veen <github@mauricevanveen.com>

--- server/monitor.go
+++ server/monitor.go
@@ -3133,8 +3133,7 @@ func (s *Server) accountDetail(jsa *jsAccount, optStreams, optConsumers, optDire
 
 	if js := s.getJetStream(); js != nil && optStreams {
 		for _, stream := range streams {
-			rgroup := stream.raftGroup()
-			ci := js.clusterInfo(rgroup)
+			ci := js.clusterInfoOfStreamAssignment(stream.streamAssignment())
 			var cfg *StreamConfig
 			if optCfg {
 				c := stream.config()
@@ -3153,8 +3152,8 @@ func (s *Server) accountDetail(jsa *jsAccount, optStreams, optConsumers, optDire
 				Mirror:  stream.mirrorInfo(),
 				Sources: stream.sourcesInfo(),
 			}
-			if optRaft && rgroup != nil {
-				sdet.RaftGroup = rgroup.Name
+			if optRaft && ci.RaftGroup != _EMPTY_ {
+				sdet.RaftGroup = ci.RaftGroup
 				sdet.ConsumerRaftGroups = make([]*RaftGroupDetail, 0)
 			}
 			if optConsumers {
